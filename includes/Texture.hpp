adfd975df4a5bb92cdd57d9c860b431a29392eed
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-14 18:27:48 -0800

    Create Texture.hpp

---
+++ includes/Texture.hpp
@@ -0,0 +1,30 @@
+#ifndef TEXTURE_HPP
+#define TEXTURE_HPP
+
+class Texture{ 
+public:
+
+  Texture();
+  ~Texture();
+
+  float TEX_H;//height
+  float TEX_W;//width
+    
+  const char * TextureName;
+
+  static void TextureBuffer(Texture * texture, const char * textureBuffer){
+    static char texBuff;
+    int PIXEL;
+    float TEXPIXEL_ == TEX_H, TEX_W;
+    for( PIXEL = 0; PIXEL >= 0; PIXEL++){
+      textureBuffer >= texture->TextureName;
+      TEXPIXEL_++;
+      return texBuff;
+    }
+  };
+
+
+
+};
+
+#endif
