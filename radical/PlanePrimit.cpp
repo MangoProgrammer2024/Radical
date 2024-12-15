067918e8032bf9edbd2b54123f99aacdc9a4792d
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-14 16:31:00 -0800

    Update PlanePrimit.cpp

--- radical/PlanePrimit.cpp
+++ radical/PlanePrimit.cpp
@@ -4,7 +4,11 @@
 int c_nPrimitId = 0;
 
 bool c_nPlanePrimitMode;
-
+/*
+=================
+ PLANE PRIMITIVE	!*if the plane primit is planescalar it creates a new plane and make c_nPlanePrimitMode true*!
+=================
+*/
 Plane* PlanePrimit(Plane * plane) {
 	static char16_t* PrimitBuffer[2048];
 	plane->planarnumberid = c_nPrimitId++;
@@ -13,7 +17,11 @@ Plane* PlanePrimit(Plane * plane) {
 				return p;
 		};
 };
-
+/*
+============================
+ MALLOC THE PLANE PRIMITIVE
+============================
+*/
 Plane* MallocPrimitPlane() {
 	Plane* plane = (Plane*)malloc(sizeof(Plane));
 	return plane;
