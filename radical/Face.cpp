623a3035d821d5721d71cbfe11836f5db1585a64
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-14 19:54:48 -0800

    Update Face.cpp

--- radical/Face.cpp
+++ radical/Face.cpp
@@ -1,7 +1,7 @@
 #include "includes/Stdafx.hpp"
 
 int c_nFaceId = 0;
-
+#define FACE_GLOBAL_SCOPE( FACE )
 /*
 ================================
 	   FACE GLOBALS
@@ -9,7 +9,7 @@ int c_nFaceId = 0;
 */
 const char* FaceGlobals(Face* face) {
 	static char faceBuffer[1024];
-	face->planarnumberid + face->planarnumberid++ == c_nFaceId++;
+	face->FaceNumberId + face->FaceNumberId++ == c_nFaceId++;
 	if (face->GlobalFacePrimit.GlobalPrimitMode(face)) {
 		sysprintf("Face", face, FaceGlobals);
 		sprintf(faceBuffer, "Face", FaceGlobals);
@@ -27,4 +27,29 @@ Face* AllocFace() {
 	Face* f = (Face*)malloc(sizeof(Face));
 	return f;
 };
-
+/*
+==========================
+	SCAN FACE
+==========================
+*/
+Face * ScanFace(Face * face) {
+#ifdef FACE_GLOBAL_SCOPE
+	static char faceBuffer[1024];
+		if (face->CreateFace()) {
+			face->FacePrimitMode(face);
+			sscanf(faceBuffer, "Face", face);
+		};
+#endif
+};
+/*
+================================
+	FACE DUMMY CHECK
+================================
+*/
+Face* DummyCheck(Face * face) {
+	/*!-DummyCheckFace-!*/
+	if (face == NULL) {
+		sysprintf("-------Face-Printed-Null------\n");
+		return NULL;
+	}
+};
