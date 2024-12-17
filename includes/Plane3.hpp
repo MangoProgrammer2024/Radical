ad7a1631292f1006bcfd3277b6b125b776c82cc4
_gynx <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-17 07:55:20 -0600

    Create Plane3.hpp

---
+++ includes/Plane3.hpp
@@ -0,0 +1,18 @@
+//Plane3.hpp
+#ifndef PLANE3_HPP
+#define PLANE3_HPP
+
+class Plane3{
+public:
+
+Vector nx;
+Vector ny;
+Vector nz;
+
+double dist = 0.5f;
+
+Plane3(nx, ny, nz, dist);
+
+};
+
+#endif
