982957bf076263224aee0ace58b5556ff33a909d
Mr.Char[/0/ ‘path’] <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-21 15:07:14 -0800

    Update brush.h

--- includes/brush.h
+++ includes/brush.h
@@ -56,6 +56,54 @@ public:
 
     bool m_bBrushPrimitMode(brush_t& m_bBrush);
 
+    /*---Brush Build Stuff---*/
+    Brush_Build( brush_t * brush, int sides, std::size_t * brushsize, const char * key, const char * value, bool buildbrush, int _numberid);
+    /*---Brush Draw Stuff---*/
+    void Brush_Draw(brush_t * b);
+    void BrushDraw_XY(brush_t * b, int iViewType);
+    void BrushDraw_ViewType(brush_t * b, int globalViewType);
+    //view types for brush
+    enum{
+        BRUSH_XY,
+        BRUSH_YX,
+        BRUSH YZ
+    }iBrushView;
+
+    brush_t&m_bBrushModuleViewObservor;
+    /*---Brush Clip Tools---*/
+    bool m_bClipBrush(brush_t * b);
+    bool m_bClipBrushPlane(brush_t * b, plane_t * p);
+    bool m_bClipBrushFace(brush_t * b, face_f * f);
+    int * Brush_ClipperTool(brush_t * b, int split_points, bool split_brush_current);
+
+    /*---Brush Manipulation Stuff---*/
+    bool Enable_BrushTextureFaceDrag(brush_t * b, face_f * f, texture_t * t);
+    void Brush_ScaleTextureWidth(brush_t * b, texture_t * texdef, int ScaleWidth);
+    void Brush_ScaleTextureHeight(brush_t * b, texture_t * texdef, int ScaleHeight);
+    void * Brush_ChangeSize(brush_t * b, std::size_t * brushNewSize);
+    bool Scale_Brush(brush_t * b);
+    bool Drag_Brush(brush_t * b);
+    bool Rotate_Brush(brush_t * b);
+
+    /*---Brush Types---*/
+    enum{ConstructiveBrush,
+         ConstructionBrush,
+         CaulkBrush,
+         HollowBrush
+    }m_bBrushType_t;
+
+    //constructive brush is a normal brush in editor and in world
+    //construction brush is a brush thats only in the editor it doesn't show or spawn in actual game
+    //caulk brush is a brush with automatic applied caulk texture on all faces !*used for stucturing map*!
+    //hollow brush is a brush that doesn't have collision in game but is still visible
+
+    bool Brush_IsConstruction(brush_t * b);
+    bool Brush_IsConstructive(brush_t * b);
+    bool Brush_IsCaulked(brush_t * b);
+    bool Brush_IsHollow(brush_t * b);
+    virtual void * Construct_Brush(brush_t * b, int mBrushType);
+
+
 };
 
 #endif // BRUSH_H
