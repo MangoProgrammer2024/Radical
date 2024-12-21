3481d719fe7a610a0ead5c2670ae7879378009ab
Mr.Char[/0/ ‘path’] <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-21 12:50:17 -0800

    Create camera.h

---
+++ radical/camera.h
@@ -0,0 +1,23 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "libs/mathlib.h"
+
+class camera_t{
+public:
+
+    camera_t();
+    virtual ~camera_t();
+
+    camera_t * current_scene;
+    camera_t * current_camera;
+
+    double Max = 1025.0f;
+    float Near = 45.0f;
+    void AddRenderScene(camera_t * c);
+    vec3_t position;
+
+};
+
+
+#endif // CAMERA_H
