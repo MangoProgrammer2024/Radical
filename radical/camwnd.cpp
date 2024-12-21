8963443c156581f8037b97c70cec3d2410ba9133
Mr.Char[/0/ ‘path’] <164366136+MangoProgrammer2024@users.noreply.github.com>
2024-12-21 13:08:26 -0800

    Add files via upload

---
+++ radical/camwnd.cpp
@@ -0,0 +1,13 @@
+#include "mainframe.h"
+#include "camwnd.h"
+#include "camera.h"
+#include "ui_mainframe.h"
+
+using namespace Ui;
+
+int windowid = 0;
+
+int _X = 50;
+int _Y = 50;
+
+std::size_t * WindowSize;
